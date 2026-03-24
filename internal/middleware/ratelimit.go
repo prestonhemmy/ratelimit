54daa23eb3265a6b75d62b1c11107c7913fdd00c
prestonhemmy <phemmy@ufl.edu>
2026-03-24 15:52:59 -0400

    refactor: pass config by pointer to middleware

--- internal/middleware/ratelimit.go
+++ internal/middleware/ratelimit.go
@@ -14,7 +14,7 @@ import (
 
 func RateLimitMiddleware(
 	limiter ratelimiter.RateLimiter,
-	cfg config.Config,
+	cfg *config.Config,
 ) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
